0b5be396c22e0f914416ea0b12372d8ca4e83adf
Mathieu BROUTIN <yakanet@gmail.com>
2020-12-13 09:55:25 +0100

    feat: add day 13

--- src/main/kotlin/advent2020/Advent13.kt
+++ src/main/kotlin/advent2020/Advent13.kt
@@ -1,9 +1,59 @@
 @file:Puzzle(2020, 13)
+
 package advent2020
 
-import common.*
+import com.google.common.math.LongMath.gcd
+import common.Puzzle
+import common.getLines
+import kotlin.math.*
 
 // Link for the exercise: https://adventofcode.com/2020/day/13
 fun main() {
-    val input = "2020/13".getText()
+    val (time, buses) = "2020/13".getLines().let { (rawTime, rawBus) ->
+        Pair(rawTime.toLong(), rawBus.split(',').mapIndexedNotNull { i, bus ->
+            if (bus != "x") Bus(i, bus.toLong()) else null
+        })
+    }
+    // Part 1
+    buses.findFirstDepartureAt(time).let { (bus, departure) ->
+        val waitingTime = departure - time
+        println(waitingTime * bus.id)
+    }
+
+    // Part 2
+    println(buses.findSequentialDeparture())
+}
+
+private fun List<Bus>.findSequentialDeparture(): Long {
+    var step = this[0].id
+    var departure = 0L
+    do {
+        departure += step
+        val busCanLeave = filter { bus -> bus.canLeave(departure + bus.index) }
+        if (busCanLeave.isNotEmpty()) {
+            step = busCanLeave.map { it.id }.reduce { a, b -> lcm(a, b) }
+        }
+    } while (busCanLeave != this)
+    return departure
+}
+
+// https://en.wikipedia.org/wiki/Least_common_multiple
+fun lcm(a: Long, b: Long): Long = abs(a * b) / gcd(a, b)
+
+private fun List<Bus>.findFirstDepartureAt(time: Long): Pair<Bus, Long> {
+    var departure = time
+    while (true) {
+        forEach { bus ->
+            if (bus.canLeave(departure)) {
+                return Pair(bus, departure)
+            }
+        }
+        departure++
+    }
+}
+
+private class Bus(val index: Int, val id: Long) {
+    fun canLeave(time: Long): Boolean {
+        return time % id == 0L
+    }
 }
