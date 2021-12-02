c97d9949532c478a4087b36c15ce8ee7044da3b9
mbroutin <mbroutin@sedona.fr>
2021-12-02 15:24:58 +0100

    feat: puzzle 2021-12-02

--- src/main/kotlin/advent2021/Advent02.kt
+++ src/main/kotlin/advent2021/Advent02.kt
@@ -9,38 +9,39 @@ import common.getLines
 fun main() {
     val input = "2021/02".getLines().toInstruction()
     solution1(input)
-    solution2(input);
+    solution2(input)
 }
 
 private fun solution1(input: List<Instruction>) {
-    val position = input.fold(Position(0, 0)) { position, instruction -> position.apply(instruction) }
-    println(position.depth * position.horizontal)
+    input.fold(Position()) { position, (operation, unit) ->
+        when (operation) {
+            Operation.DOWN -> position.copy(depth = position.depth + unit)
+            Operation.UP -> position.copy(depth = position.depth - unit)
+            Operation.FORWARD -> position.copy(horizontal = position.horizontal + unit)
+        }
+    }.apply {
+        println(depth * horizontal)
+    }
 }
 
 private fun solution2(input: List<Instruction>) {
-    val position = input.fold(Position2(0, 0, 0)) { position, instruction -> position.apply(instruction) }
-    println(position.depth * position.horizontal)
+    input.fold(Position()) { position, (operation, unit) ->
+        when (operation) {
+            Operation.DOWN -> position.copy(aim = position.aim + unit)
+            Operation.UP -> position.copy(aim = position.aim - unit)
+            Operation.FORWARD -> position.copy(
+                horizontal = position.horizontal + unit,
+                depth = position.depth + position.aim * unit
+            )
+        }
+    }.apply {
+        println(depth * horizontal)
+    }
 }
 
 private enum class Operation { UP, DOWN, FORWARD }
 private data class Instruction(val operation: Operation, val unit: Int)
-private data class Position(val horizontal: Int, val depth: Int) {
-    fun apply(instruction: Instruction): Position = when (instruction.operation) {
-        Operation.DOWN -> copy(depth = depth + instruction.unit)
-        Operation.UP -> copy(depth = depth - instruction.unit)
-        Operation.FORWARD -> copy(horizontal = horizontal + instruction.unit)
-    }
-}
+private data class Position(val horizontal: Int = 0, val depth: Int = 0, val aim: Int = 0)
 
-private data class Position2(val horizontal: Int, val depth: Int, val aim: Int) {
-    fun apply(instruction: Instruction): Position2 = when (instruction.operation) {
-        Operation.DOWN -> copy(aim = aim + instruction.unit)
-        Operation.UP -> copy(aim = aim - instruction.unit)
-        Operation.FORWARD -> copy(horizontal = horizontal + instruction.unit, depth = depth + aim * instruction.unit)
-    }
-}
-
-private fun List<String>.toInstruction(): List<Instruction> {
-    return this.map { it.split(" ") }
-        .map { (op, unit) -> Instruction(Operation.valueOf(op.uppercase()), unit.toInt()) }
-}
+private fun List<String>.toInstruction() = map { it.split(" ") }
+    .map { (operation, unit) -> Instruction(Operation.valueOf(operation.uppercase()), unit.toInt()) }
